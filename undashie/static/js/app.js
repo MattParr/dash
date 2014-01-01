8b8604edbf7b5cb884576c2878fd50ea42780a12
Rui Carmo <rui.carmo@gmail.com>
2014-01-01 12:18:25 +0000

    First pass at loading

--- undashie/static/js/app.js
+++ undashie/static/js/app.js
@@ -1,3 +1,4 @@
+/* Model */
 function Dashboard() {
   var self = $.observable(this);
 
@@ -6,23 +7,52 @@ function Dashboard() {
   self.widget_base_dimensions = [300, 360];
   self.numColumns = 4;
   self.contentWidth = (self.widget_base_dimensions[0] + self.widget_margins[0] * 2) * self.numColumns;
+
+  $.get("/widgets/list.json", function(data){
+    console.log(data);
+    $.each(data.widgets, function(i) {
+      item = data.widgets[i];
+      self.widgets.push(item);
+      self.trigger("add", item);
+    })
+    console.log(self.widgets);
+  });
 };
 
+
+/* Presenter */
 (function() {
 
   console.log("Started.");
 
+  var templates = {};
+
   dash = window.dashboard = new Dashboard();
+
+  root = $('.gridster');
   $(document).ready(function(){
-    $('.gridster').width(dash.contentWidth);
+    root.width(dash.contentWidth);
     $('.gridster ul').gridster({
       widget_margins: dash.widget_margins,
       widget_base_dimensions: dash.widget_base_dimensions,
       avoid_overlapped_widgets: true
     });
-
     console.log("Gridster set.");    
-  })
+  });
 
+
+  dash.on("add", function(item) {
+    if(!(item.kind in templates)) {
+      /* Load templates,styles and behavior */
+      $('head').append('<link rel="stylesheet" type="text/css" href="widgets/' + item.kind + '.css">');
+      $.get('widgets/' + item.kind + '.html', function(data) {
+        templates[item.kind] = data;
+        $.getScript('widgets/' + item.kind + '.js', function() {
+          console.log("Loaded " + item.kind);
+          self.trigger("init", item);
+        })
+      })
+    }
+  });
 
 }).call(this);

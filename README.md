c9e09eaa91cead18cade4cb89d704ec40783d590
Rui Carmo <rui.carmo@gmail.com>
2014-01-11 17:41:25 +0000

    first pass at a server-side queue

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Undashie
 
-This is a modified version of PyDashie, which is in turn a port of [Dashing](https://github.com/Shopify/dashing>) by
-[Shopify](http://www.shopify.com/>) to Python 2.7.
+This started out as a modified version of PyDashie, which is in turn a port of [Dashing](https://github.com/Shopify/dashing) by [Shopify](http://www.shopify.com/).
+
+However, it's now an almost (but not quite) completely different beast, save for the CSS (which I happen to like).
--- routes/events.py
+++ routes/events.py
@@ -6,9 +6,11 @@ from bottle import app, request, response, route, redirect, static_file, view
 
 from utils import path_for
 from config import settings
+from miniredis.client import RedisClient
 
 log = logging.getLogger()
 
+r = RedisClient(**settings.redis)
 
 def pack(d):
     buffer = ''
@@ -20,22 +22,46 @@ def pack(d):
 
 @route('/event')
 def event():
-    if not request.get_cookie("client-id"):
-        response.set_cookie("client-id", str(uuid.uuid4()))
+    client_id = request.get_cookie("client-id")
+    if not client_id:
+        client_id = str(uuid.uuid4())
+        response.set_cookie("client-id", client_id)
+        # add this to the clients list
+        r.rpush("dash:clients", client_id)
+        # create a new initialization event
+        r.setex("event:%s" % client_id, 5, json.dumps({"client-id": client_id}))
+        # ...and add a reference to it to a new client queue
+        r.rpush("client:%s" % client_id, "event:%s" % client_id)
+    # reset the queue timeout
+    r.expire("client:%s" % client_id, 120)
+    # TODO: remove item from dash:clients if queue expired
+
+    # now fetch all the events this client is entitled to
+    events = [{
+        'retry': '5000',
+        'id'   : uuid.uuid4(),
+        'event': 'tick',
+        'data' : str(datetime.datetime.now())
+    }]
+    for i in range(r.llen("client:%s" % client_id))
+        # pop each ID from the queue and grab the corresponding data
+        event_id = r.lpop("client:%s") 
+        if not event_id:
+            break
+        ev = r.get(event_id)
+        if not ev:
+            break
+        try:
+            ev = json.loads(ev)
+        except Exception as e:
+            log.debug("Could not parse %s" % ev)
+            break
+        for k in ev.keys:
+            events.append({'id': event_id, 'event': k, 'data': json.dumps(ev[k])})
 
     log.debug(map(lambda x: (x,request.headers[x]),request.headers.keys()))
     response.headers['content-type'] = 'text/event-stream'
-    msg = {
-        'retry': '5000',
-        'event': 'tick',
-        'data': str(datetime.datetime.now()),
-    }
-
-    if 'Last-Event-Id' in request.headers:
-        msg['id'] = str(int(request.headers['Last-Event-Id']) + 1)
-    else:
-        msg['id'] = "0"
-
-    buffer = pack(msg)
-    print buffer
+    buffer = ''
+    for e in events:
+        buffer += pack(e)
     return buffer

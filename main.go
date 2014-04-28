5f3f58d5f7d17bbfbe92d333f0ea396f46a8ecc2
Rui Carmo <rui.carmo@gmail.com>
2014-04-28 18:24:09 +0100

    added basic EventSource test

--- main.go
+++ main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"fmt"
+	eventsource "github.com/antage/eventsource/http"
 	"github.com/bmizerany/pat"          // Sinatra-like router
 	"github.com/fiorix/go-web/autogzip" // gzip support
 	//"github.com/hoisie/mustache"  Mustache-like templating engine
 	"log"
 	"net/http"
 	"path"
+	"time"
+    "strconv"
 )
 
 var listenPort int = 8080
@@ -16,10 +19,26 @@ var staticPath string = "static"
 func Api(w http.ResponseWriter, r *http.Request) {
 }
 
+func Source(es eventsource.EventSource) {
+	id := 1
+	for {
+		es.SendMessage("tick", "tick-event", strconv.Itoa(id))
+		id++
+		time.Sleep(5 * time.Second)
+	}
+}
+
 func main() {
-	m := pat.New()
-	m.Get("/api/:id", http.HandlerFunc(Api))
-	http.Handle("/api", m)
+
+	es := eventsource.New(nil, nil)
+	defer es.Close()
+
+	http.Handle("/event", es)
+	go Source(es)
+
+	r := pat.New()
+	r.Get("/api/:id", http.HandlerFunc(Api))
+	http.Handle("/api", r)
 
 	// Serve static files
 	http.Handle("/static", autogzip.Handle(http.FileServer(http.Dir(staticPath))))

a0bb7f55c6ebe2ad4815b350644019b8f1c740bb
Rui Carmo <rui.carmo@gmail.com>
2014-04-28 17:45:35 +0100

    route tweaking

--- main.go
+++ main.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"fmt"
-	"github.com/bmizerany/pat" // Sinatra-like router
-    "github.com/fiorix/go-web/autogzip" // gzip support
+	"github.com/bmizerany/pat"          // Sinatra-like router
+	"github.com/fiorix/go-web/autogzip" // gzip support
 	//"github.com/hoisie/mustache"  Mustache-like templating engine
 	"log"
-    "path"
 	"net/http"
+	"path"
 )
 
 var listenPort int = 8080
@@ -21,13 +21,13 @@ func main() {
 	m.Get("/api/:id", http.HandlerFunc(Api))
 	http.Handle("/api", m)
 
-    // Serve static files
-    http.Handle("/static", autogzip.Handle(http.FileServer(http.Dir(staticPath))))
+	// Serve static files
+	http.Handle("/static", autogzip.Handle(http.FileServer(http.Dir(staticPath))))
 
-    // Serve site root
-    http.HandleFunc("/", autogzip.HandleFunc(func(w http.ResponseWriter, r *http.Request) {
-        http.ServeFile(w, r, path.Join(staticPath, "index.html"))
-    }))
+	// Serve site root
+	http.HandleFunc("/", autogzip.HandleFunc(func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, path.Join(staticPath, "index.html"))
+	}))
 
 	err := http.ListenAndServe(fmt.Sprintf(":%d", listenPort), nil)
 	if err != nil {

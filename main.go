2114e3ea44c73b4324cce06bdb635467ab79fa29
Rui Carmo <rui.carmo@gmail.com>
2014-04-28 17:45:16 +0100

    route tweaking

--- main.go
+++ main.go
@@ -3,13 +3,15 @@ package main
 import (
 	"fmt"
 	"github.com/bmizerany/pat" // Sinatra-like router
+    "github.com/fiorix/go-web/autogzip" // gzip support
 	//"github.com/hoisie/mustache"  Mustache-like templating engine
 	"log"
+    "path"
 	"net/http"
 )
 
 var listenPort int = 8080
-var staticPath string = "/static/"
+var staticPath string = "static"
 
 func Api(w http.ResponseWriter, r *http.Request) {
 }
@@ -17,8 +19,16 @@ func Api(w http.ResponseWriter, r *http.Request) {
 func main() {
 	m := pat.New()
 	m.Get("/api/:id", http.HandlerFunc(Api))
-	http.Handle("/", m)
-    http.Handle(staticPath, http.StripPrefix(staticPath, http.FileServer(http.Dir("." + staticPath))))
+	http.Handle("/api", m)
+
+    // Serve static files
+    http.Handle("/static", autogzip.Handle(http.FileServer(http.Dir(staticPath))))
+
+    // Serve site root
+    http.HandleFunc("/", autogzip.HandleFunc(func(w http.ResponseWriter, r *http.Request) {
+        http.ServeFile(w, r, path.Join(staticPath, "index.html"))
+    }))
+
 	err := http.ListenAndServe(fmt.Sprintf(":%d", listenPort), nil)
 	if err != nil {
 		log.Fatal(err)
